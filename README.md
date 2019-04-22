b57600c467aec51367fbde3dc419a9668415af67
Richard Van Camp <richard.vancamp@gmail.com>
2019-04-22 21:15:17 +0300

    Version 2.1 and release script

--- README.md
+++ README.md
@@ -14,28 +14,30 @@ npm install use-epics
 
 ## Api
 
-`useEpics` takes, at a minimum, a map of actions and initial state. 
+`useEpics` takes, at a minimum, a map of actions and initial state.
 
-Actions use immer under the covers by default, so you can either return a new state (as if not using `immer`) or you can just update properties and not return a new state as `immer` does this for you. These actions will update state via a single useReducer hook which you do not have to worry about. If an action returns `undefined`, the state update will be skipped. This can be used to trigger a side-effect. Types are also derived for your actions so you do not need to provide your own action types. 
+Actions use immer under the covers by default, so you can either return a new state (as if not using `immer`) or you can just update properties and not return a new state as `immer` does this for you. These actions will update state via a single useReducer hook which you do not have to worry about. If an action returns `undefined`, the state update will be skipped. This can be used to trigger a side-effect. Types are also derived for your actions so you do not need to provide your own action types.
 
 The optional third argument is an list of epics, which are called in order after the state is updated by an action with the observables of the action and updated state.
 
 The final optional argument is an options object through which you can pass an initializer as the third argument to `useReducer` inside of `useEpics`, allowing lazy initialization of state. See the [React docs](https://reactjs.org/docs/hooks-reference.html#lazy-initialization) for more information in lazy initialization. You may also pass a `{immer: false}` option if you would rather not use immer.
 
-
 ### `useEpic`
 
-```
-
-const [state, actions] = useEpics(createActions, initialState, [epic1, epic2], options)
-
+```js
+const [state, actions] = useEpics(
+    createActions,
+    initialState,
+    [epic1, epic2],
+    options
+)
 ```
 
 ### `options`
 
-```
+```js
 
-{ 
+{
   // defaults to  undefined
   init: (initial: number) => ({count}),
   // defaults to true
@@ -46,26 +48,50 @@ const [state, actions] = useEpics(createActions, initialState, [epic1, epic2], o
 
 ## Example
 
-```
+```jsx
 
-import React from 'react' import { interval, empty } from 'rxjs' import { map, switchMap } from 'rxjs/operators' import { Epic, useEpics, ofType } from 'use-epics'
+import React from 'react' import { interval, empty } from 'rxjs'
+import { map, switchMap } from 'rxjs/operators'
+import { Epic, useEpics, ofType } from 'use-epics'
 
 const initialState = { count: 0, delay: null }
 
 interface State { count: number delay: number | null }
 
-const createActions = (state: State) => ({ reset: () => initialState, inc() { state.count++ }, dec() { state.count-- }, start(delay: number) { state.delay = delay }, stop() { state.delay = null } })
+const createActions = (state: State) =>
+  ({
+    reset: () => initialState,
+    inc() { state.count++ },
+    dec() { state.count-- },
+    start(delay: number) { state.delay = delay },
+    stop() { state.delay = null }
+  })
 
 const counterEpic: Epic<State, ReturnType<typeof createActions>> = ( action$,
   state$, actions ) => action$.pipe(
     ofType('start', 'stop'),
     switchMap(({ type }) => {
-      const delay = state$.value.delay return type === 'start' && delay ? interval(delay).pipe(map(actions.inc)) : empty() }) )
+      const delay = state$.value.delay
+      return type === 'start' && delay
+        ? interval(delay).pipe(map(actions.inc))
+        : empty()
+    })
+  )
 
-export function App() { const [{count}, { inc, dec, reset, start, stop }] = useEpics( createActions, initialState, [counterEpic] )
+export function App() {
+  const [{count}, { inc, dec, reset, start, stop }] =
+    useEpics( createActions, initialState, [counterEpic] )
 
-return ( <div className="App"> <div>{count}</div> <button onClick={() => inc()}>increment</button> <button onClick={() => dec()}>decrement</button> <button onClick={() => reset()}>reset</button> <button onClick={() => start(1000)}>start</button> <button onClick={() => stop()}>stop</button> </div> ) }
-
-```
+  return (
+    <div className="App">
+      <div>{count}</div>
+      <button onClick={() => inc()}>increment</button>
+      <button onClick={() => dec()}>decrement</button>
+      <button onClick={() => reset()}>reset</button>
+      <button onClick={() => start(1000)}>start</button>
+      <button onClick={() => stop()}>stop</button>
+    </div>
+    )
+}
 
 ```
--- demo/App.tsx
+++ demo/App.tsx
@@ -1,69 +1,69 @@
-import React, { useRef, useEffect } from 'react'
-import { interval, empty } from 'rxjs'
-import { ajax } from 'rxjs/ajax'
-import { map, switchMap, tap, ignoreElements } from 'rxjs/operators'
-import { Epic, useEpics, ofType } from '../src'
+import React, {useRef, useEffect} from "react"
+import {interval, empty} from "rxjs"
+import {ajax} from "rxjs/ajax"
+import {map, switchMap, tap, ignoreElements} from "rxjs/operators"
+import {Epic, useEpics, ofType} from "../src"
 
 const initialState = {
-  num: 0,
-  delay: null,
-  dogSrc: null
+    num: 0,
+    delay: null,
+    dogSrc: null
 }
 
 interface State {
-  num: number
-  delay: number | null
-  dogSrc: string | null
+    num: number
+    delay: number | null
+    dogSrc: string | null
 }
 
 const createActions = (state: State) => ({
-  reset: () => initialState,
-  inc() {
-    state.num++
-  },
-  dec() {
-    state.num--
-  },
-  start() {
-    state.delay = 1000
-  },
-  stop() {
-    state.delay = null
-  },
-  getDog: () => undefined,
-  setDog(dogSrc: string) {
-    state.dogSrc = dogSrc
-  }
+    reset: () => initialState,
+    inc() {
+        state.num++
+    },
+    dec() {
+        state.num--
+    },
+    start() {
+        state.delay = 1000
+    },
+    stop() {
+        state.delay = null
+    },
+    getDog: () => undefined,
+    setDog(dogSrc: string) {
+        state.dogSrc = dogSrc
+    }
 })
 
 const counterEpic: Epic<State, ReturnType<typeof createActions>> = (
-  action$,
-  state$,
-  actions
+    action$,
+    state$,
+    actions
 ) =>
-  action$.pipe(
-    ofType('start', 'stop'),
-    switchMap(({ type }) => {
-      const delay = state$.value.delay
-      return type === 'start' && delay
-        ? interval(delay).pipe(map(actions.inc))
-        : empty()
-    })
-  )
+    action$.pipe(
+        ofType("start", "stop"),
+        switchMap(({type}) => {
+            const delay = state$.value.delay
+            return type === "start" && delay
+                ? interval(delay).pipe(map(actions.inc))
+                : empty()
+        })
+    )
 
 const dogFetchEpic: Epic<State, ReturnType<typeof createActions>> = (
-  action$,
-  _,
-  actions
+    action$,
+    _,
+    actions
 ) =>
-  action$.pipe(
-    ofType('getDog'),
-    switchMap(() => {
-      return ajax
-        .getJSON('https://dog.ceo/api/breeds/image/random')
-        .pipe(map((res: any) => actions.setDog(res.message)))
-    })
-  )
+    action$.pipe(
+        ofType("getDog"),
+        switchMap(() => {
+            return ajax
+                .getJSON("https://dog.ceo/api/breeds/image/random")
+                .pipe(map((res: any) => actions.setDog(res.message)))
+        })
+    )
 
 /*const loggerEpic: Epic<State, ReturnType<typeof createActions>> = (
   action$,
@@ -85,32 +85,32 @@ const dogFetchEpic: Epic<State, ReturnType<typeof createActions>> = (
 const epics = [counterEpic, dogFetchEpic]
 
 export function App() {
-  const [state, { inc, dec, reset, start, stop, getDog }] = useEpics(
-    createActions,
-    initialState,
-    epics
-  )
-  // track how many times methods change (should never be more than zero)
-  const actionChanges = useRef(-1)
-  useEffect(() => {
-    actionChanges.current++
-  }, [inc, dec, reset, start, stop, getDog])
+    const [state, {inc, dec, reset, start, stop, getDog}] = useEpics(
+        createActions,
+        initialState,
+        epics
+    )
+    // track how many times methods change (should never be more than zero)
+    const actionChanges = useRef(-1)
+    useEffect(() => {
+        actionChanges.current++
+    }, [inc, dec, reset, start, stop, getDog])
 
-  return (
-    <div className="App">
-      <div data-testid="count">{state.num}</div>
-      <button onClick={() => inc()}>increment</button>
-      <button onClick={() => dec()}>decrement</button>
-      <button onClick={() => reset()}>reset</button>
-      <div>
-        <button onClick={() => start()}>start</button>
-        <button onClick={() => stop()}>stop</button>
-      </div>
-      <button onClick={() => getDog()}>dog</button>
-      <div>{state.dogSrc && <img src={state.dogSrc} />}</div>
-      <label>
-        actions created: <span>{actionChanges.current}</span>
-      </label>
-    </div>
-  )
+    return (
+        <div className="App">
+            <div data-testid="count">{state.num}</div>
+            <button onClick={() => inc()}>increment</button>
+            <button onClick={() => dec()}>decrement</button>
+            <button onClick={() => reset()}>reset</button>
+            <div>
+                <button onClick={() => start()}>start</button>
+                <button onClick={() => stop()}>stop</button>
+            </div>
+            <button onClick={() => getDog()}>dog</button>
+            <div>{state.dogSrc && <img src={state.dogSrc} />}</div>
+            <label>
+                actions created: <span>{actionChanges.current}</span>
+            </label>
+        </div>
+    )
 }
